4c054a91187aa952956a5bd6049223800b0464b8
sami suliman <sami.bco@gmail.com>
2019-10-05 20:18:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,11 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @product = @num1.to_i * @num2.to_i
+    "#{@product}"
+  end
 
 end
